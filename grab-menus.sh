4132fbe096a83451bad310d87226608867c395e7
Dale Nicholson <dale.nicholson2@gmail.com>
2019-02-20 14:39:57 -0600

    updates and adding more files

--- grab-menus.sh
+++ grab-menus.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Get desktop files for applications that were not installed with the app
+# (ones I had to create and set up myself)
+cp /usr/share/applications/ham* ./
+
 #Get directory files I had to add/create or borrow from other distros
 cp /usr/share/desktop-directories/ubuntustudio* ./
 cp /usr/share/desktop-directories/ham* ./
