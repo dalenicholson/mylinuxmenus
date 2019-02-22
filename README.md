b0c8a54763a1eeedbc6ac8765d573457712b1341
Dale Nicholson <dale.nicholson2@gmail.com>
2019-02-22 07:51:42 -0600

    adding more files and updates

--- README.md
+++ README.md
@@ -2,8 +2,10 @@ This is a place to store my customized linux menus since they are such a pain to
 
 I liked the setup from Ubuntu Studio for video, graphics, audio especially and copied those.  I also set up a bunch of new ones for Ham radio related stuff since I wanted these grouped more like the way Andy did his on the Andy's Ham Radio Linux distro, but he didn't implement using the freedesktop.org standards so I had to create my own instead of borrowing his.
 
+I also liked the menus on Parrot Security OS so I utilized those too.  Menus here were a lot easier to duplicate since they followed the freedesktop standard, I was able to get the menu files directly instead of having to create them from scratch.  They also had a couple basic shells to use to call packages that have to run as root that I liked and used (one for cmd line and one for graphical programs, menuexec and menuexecg).  I used Parrot's desktop files for the apps too, as they already had the parrot categories and the shells being called.  Each time I install one of the apps from parrot I copy over the .dekstop file for that program rather than keeping the default installed with the program.
 
-I'll add more details to this later, just wanting to get these backed up for now.
+I'll add more details to this later, just wanting to get these backed up for now.  
+
 
 You can look at the shell if you want to know what directories I had these various files located.  Note that you can also put these into user specific folders in your home directory if you want.
 
--- grab-menus.sh
+++ grab-menus.sh
@@ -4,18 +4,63 @@
 # (ones I had to create and set up myself)
 cp /usr/share/applications/ham* ./
 
+#get parrot versions of desktop files
+#note, the parrot files have the correct categories to match the custom menus
+#when apps install, they add their own normal one too.
+cp /usr/share/applications/parrot* ./
+
 #Get directory files I had to add/create or borrow from other distros
 cp /usr/share/desktop-directories/ubuntustudio* ./
 cp /usr/share/desktop-directories/ham* ./
 cp /usr/share/desktop-directories/Ham* ./
 cp /usr/share/desktop-directories/Electro* ./
 
+#Get directory files that came from Parrot
+cp /usr/share/desktop-directories/top10* ./
+cp /usr/share/desktop-directories/Parrot* ./
+cp /usr/share/desktop-directories/parrot* ./
+cp /usr/share/desktop-directories/anon* ./
+cp /usr/share/desktop-directories/cryptography* ./
+cp /usr/share/desktop-directories/0* ./
+cp /usr/share/desktop-directories/1* ./
+
 #Get ubuntustudio icons
 cp /usr/share/icons/ubuntustudio-* ./
+
+#Get Parrot icons
+cp /usr/share/icons/parrot* ./
+cp /usr/share/icons/zeal.png ./
+cp /usr/share/icons/social* ./
+cp /usr/share/icons/vuln* ./
+cp /usr/share/icons/wire* ./
+cp /usr/share/icons/web* ./
+cp /usr/share/icons/system* ./
+cp /usr/share/icons/stress* ./
+cp /usr/share/icons/start* ./
+cp /usr/share/icons/sniff* ./
+cp /usr/share/icons/reverse* ./
+cp /usr/share/icons/report* ./
+cp /usr/share/icons/pass* ./
+cp /usr/share/icons/most* ./
+cp /usr/share/icons/mainta* ./
+cp /usr/share/icons/info* ./
+cp /usr/share/icons/hardwa* ./
+cp /usr/share/icons/forensic* ./
+cp /usr/share/icons/exploi* ./
+cp /usr/share/icons/databa* ./
+cp /usr/share/icons/crypto* ./
+cp /usr/share/icons/automo* ./
+cp /usr/share/icons/anon* ./
+
+#maybe grab these icons too  /home/dale/andy/squashfs-root/usr/share/icons/maia/apps/scalable/
+# and these category icons  /home/dale/andy/squashfs-root/usr/share/icons/maia/categories/scalable/
 
 #Get custom menu definitions to be merged
 cp /etc/xdg/menus/applications-merged/* ./
 
+#grab two shell scripts that parrot uses that I copied to /usr/bin to 
+#avoid having to update all the desktop files with different exec lines.
+cp /usr/bin/menu* ./
 
 #git push origin master
 
